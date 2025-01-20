f71323d8598d59b09f23b2c8623a4ff97b577288
phucthien1811 <jack0baohbocon@gmail.com>
2025-01-20 16:33:15 +0700

    Add files via upload

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import "../scss/app.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+// AdminKit (required)
+import "./modules/bootstrap";
+import "./modules/sidebar";
+import "./modules/theme";
+import "./modules/feather";
+
+// Charts
+import "./modules/chartjs";
+
+// Forms
+import "./modules/flatpickr";
+
+// Maps
+import "./modules/vector-maps";
