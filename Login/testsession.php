ec50fba9bf9f8312b54298f20b9d819f0ecc1b19
hao888-dev <5001104043@student.hcmue.edu.vn>
2025-03-01 20:11:45 +0700

    Delete Login directory

--- Login/testsession.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if (isset($_SESSION['user_name'])) {
-    echo "Xin chào, " . htmlspecialchars($_SESSION['user_name']) . "!";
-} else {
-    echo "Bạn chưa đăng nhập.";
-}
